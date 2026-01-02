532aac5278f9df9d1cde0c72bf4a71258aa27f9e
kira23j <kirubelmamuyesahle@gmail.com>
2026-01-02 19:49:43 +0300

    adding another method

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,9 +2,15 @@
 
 class Program
 {
+    static void Main1()
+    {
+        Console.WriteLine("This is Main-one");
+    }
+    
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World");
+        Main1();
     }
 
 
