1ff1284e78030c3d3c91353eaaea1a71e959c3b9
kira23j <kirubelmamuyesahle@gmail.com>
2026-01-06 17:55:25 +0300

    add Customer class with PrintFullName method and Main program

--- ClassesPractice/Program.cs
+++ ClassesPractice/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System;
+
+class Customer{
+    string _firstName;
+    string _lastName;
+
+    public Customer(string firstName, string lastName){
+        this._firstName = firstName;
+        this._lastName = lastName; 
+    }
+    public void PrintFullName(){
+        Console.WriteLine("Full-Name {0} {1}", this._firstName, this._lastName);
+}
+}
+
+class Program{
+    public static void Main(){
+        Customer C1 = new Customer("John", "Doe");
+        C1.PrintFullName();
+    }
+}
