88946469795d10e90fd2d015ad79d388dde669b8
kira23j <kirubelmamuyesahle@gmail.com>
2026-01-06 15:17:32 +0300

    refactor Main methods and add EvenNumbers functionality

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,14 +4,68 @@ class Program
 {
     static void Main1()
     {
-        Console.WriteLine("This is Main-one");
+        Console.WriteLine("lets know each other, your first name: ");
+        string FirstName = Console.ReadLine();
+
+        Console.WriteLine("lets know each other, your last name: ");
+        string LastName = Console.ReadLine();
+        Console.WriteLine("Hello there! {0}, {1}", FirstName, LastName);
     }
-    
+    static void Main2()
+    {
+    int [] Numbers = new int [3];
+    Numbers[0] = 1; 
+    Numbers[1] = 2;
+    Numbers[2] = 3; 
+    int i = 0;
+// for each loop
+    foreach (int num in Numbers)
+    {
+        Console.WriteLine(num);
+    }
+// for loop 
+    for (int j = 0; j < Numbers.Length; j++)
+    {
+        Console.WriteLine(Numbers[j]);
+    }
+// while loop
+    while (i < Numbers.Length)
+    {
+        Console.WriteLine(Numbers[i]);
+        i++;      
+
+    }
+    }
+public void EvenNumbers(){
+    int Start = 0;
+    while (Start <= 20)
+    {
+        if (Start % 2 == 0)
+        {
+            Console.WriteLine(Start);
+        }
+        Start++;
+    }
+}
+public static void NextEvenNumers(){
+    int Start = 20;
+    while (Start <= 50)
+    {
+        if (Start % 2 == 0)
+        {
+            Console.WriteLine(Start);
+        }
+        Start++;
+    }
+}
+  
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World");
         Main1();
-    }
-
-
+        Main2();
+        Program p = new Program();
+        p.EvenNumbers();    
+       Program.NextEvenNumers();
+}
 }
