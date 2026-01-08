53fe60600109f6b975b4d144cf376c7e3226104e
kira23j <kirubelmamuyesahle@gmail.com>
2026-01-08 14:35:01 +0300

    add Circle class with CalculateArea method and update Main program

--- ClassesPractice/Program.cs
+++ ClassesPractice/Program.cs
@@ -13,9 +13,34 @@ class Customer{
 }
 }
 
+class Circle
+{   
+    static float _PI = 3.14F;
+    int _Radius;
+
+    public Circle(int Radius)
+    {
+        this._Radius = Radius;
+    }
+
+    public float CalculateArea(){
+        return Circle._PI * this._Radius * this._Radius;
+    }
+
+}
+
 class Program{
     public static void Main(){
+        // customer
         Customer C1 = new Customer("John", "Doe");
         C1.PrintFullName();
+        // circles
+        Circle Cir1= new Circle(5);
+        float Area1 = Cir1.CalculateArea();
+        Console.WriteLine("Area of Circle1: {0}", Area1);
+
+        Circle Cir2= new Circle(6);
+        float Area2 = Cir2.CalculateArea();
+        Console.WriteLine("Area of Circle2: {0}", Area2);
     }
 }
